7fd403351bc233d66bf99b5ce4b30394bb47b22a
Patrick Jahns <github@patrickjahns.de>
2021-01-09 11:31:35 +0100

    build: improve build process with go generate for graphql

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,5 +1,8 @@
 package graph
 
+// use go generate to regenerate this file (see: https://gqlgen.com/getting-started/#finishing-touches )
+//go:generate go run github.com/99designs/gqlgen
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
